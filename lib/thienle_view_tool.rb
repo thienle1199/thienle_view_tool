d8a1ec7e3084d124bc4d54da05b1f1ef4b4d43c6
Thiienle <thienle.luong@gmail.com>
2018-03-29 14:31:40 +0700

    Initialize commit for Thienle view tool

---
+++ lib/thienle_view_tool.rb
@@ -0,0 +1,6 @@
+require "thienle_view_tool/version"
+require "thienle_view_tool/renderer"
+
+module ThienleViewTool
+  # Your code goes here...
+end
---
+++ lib/thienle_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ThienleViewTool
+  VERSION = "0.1.0"
+end
